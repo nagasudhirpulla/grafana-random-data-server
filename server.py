bc4fb0e1226af0d9fcc138bc2eab3ce28a1073ad
unknown <nagasudhirpulla@gmail.com>
2022-09-18 16:02:00 +0530

    server code

---
+++ server.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+from dateutil import parser
+import datetime as dt
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello World!!!"
+
+
+@app.route('/', methods=["POST"])
+def query():
+    # get the request JSON
+    req = request.get_json()
+    print(req)
+
+    # get the start and end times
+    startTime = parser.parse(req["TimeRange"]["From"])
+    endTime = parser.parse(req["TimeRange"]["To"])
+    
+    # get the payload from json
+    # payloadStr = req["JSON"]["payload"]
+
+    # derive the response series name
+    seriesName = req["RefID"]
+    if "alias" in req["JSON"].keys():
+        alias = req["JSON"]["alias"].strip()
+        if not len(alias) == 0:
+            seriesName = alias
+
+    # generate the response object
+    resp = {"data": [
+        {"name": "time",
+         "values": [
+            1000*dt.datetime.timestamp(startTime),
+            1000*dt.datetime.timestamp(endTime)
+            ], "labels": None},
+        {"name": seriesName, "values": [5, 10], "labels": None}
+    ]}
+    print(resp)
+    return resp
+
+
+app.run(host="0.0.0.0", port=8080, debug=True)
